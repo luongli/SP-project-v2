9b5822f0e5a14460550fcf3648ec6a4ba750be43
Sam Hoang <samhv.ict@gmail.com>
2016-11-28 23:45:43 +0700

    Remove unusable code

--- module/memberAdministration/member-admin.js
+++ module/memberAdministration/member-admin.js
@@ -19,17 +19,15 @@ var db = require('./model/db');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+
+
 var port = process.env.PORT || 8000;        // set our port
 
-// ROUTES FOR OUR API
-// =============================================================================
-var router = express.Router();              // get an instance of the express Router
 
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-
+// add Restful Controller
 app.use('/api/', require('./controller/UsersRestController'));
 
-// START THE SERVER
-// =============================================================================
+
+// start
 app.listen(port);
 console.log('Magic happens on port ' + port);
