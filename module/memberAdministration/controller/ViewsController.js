d1fdaf8a403549963444f856e8aca455a52449aa
LuongNV <luongintel2009@gmail.com>
2016-11-29 23:32:41 +0700

    Add: login page

--- module/memberAdministration/controller/ViewsController.js
+++ module/memberAdministration/controller/ViewsController.js
@@ -9,4 +9,11 @@ ViewsController.get('/home', function (req, res) {
   	//res.end('ok');
 });
 
+ViewsController.get('/login', function (req, res) {
+    res.render('login/login.jade',
+  		{ title : 'Login' }
+  	);
+});
+
+
 module.exports = ViewsController;
