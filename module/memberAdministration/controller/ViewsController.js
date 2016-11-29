d20286c35b9ec47af1b4672b5e436cd9392947fe
LuongNV <luongintel2009@gmail.com>
2016-11-29 22:35:53 +0700

    Mod: refactor layout

--- module/memberAdministration/controller/ViewsController.js
+++ module/memberAdministration/controller/ViewsController.js
@@ -3,7 +3,7 @@ var express = require('express')
 var ViewsController = express.Router();
 
 ViewsController.get('/home', function (req, res) {
-    res.render('index.jade',
+    res.render('home/index.jade',
   		{ title : 'Home' }
   	)
   	//res.end('ok');
