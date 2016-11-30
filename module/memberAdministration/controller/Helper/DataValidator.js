cbeeb9e0298c7ce7f2b4aa3be0caae09d40776ab
Sam Hoang <samhv.ict@gmail.com>
2016-11-30 10:49:35 +0700

    Add| emailValidator & nameValidator

--- module/memberAdministration/controller/Helper/DataValidator.js
+++ module/memberAdministration/controller/Helper/DataValidator.js
@@ -1,3 +1,18 @@
 /**
  * Created by samhv on 11/30/16.
  */
+'use strict'
+
+var DataValidator = function () {
+    this.emailValidator = function (email) {
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+    }
+
+    this.nameValidator = function (name) {
+        var re = /^[a-zA-Z ]+$/;
+        return re.test(name)
+    }
+}
+
+module.exports = DataValidator;
