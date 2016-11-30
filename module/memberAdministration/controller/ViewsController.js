8541aa4973e8785680619d826060f7659709831c
LuongNV <luongintel2009@gmail.com>
2016-11-30 10:28:49 +0700

    Mod: refactor login page

--- module/memberAdministration/controller/ViewsController.js
+++ module/memberAdministration/controller/ViewsController.js
@@ -10,9 +10,23 @@ ViewsController.get('/home', function (req, res) {
 });
 
 ViewsController.get('/login', function (req, res) {
-    res.render('login/login.jade',
-  		{ title : 'Login' }
-  	);
+    var data = {
+      title : 'Login',
+      next_link: '/signup',
+      text_of_link: 'Create new account',
+      script: '/static/js/login.js'
+    }
+    res.render('login/login.jade', data);
+});
+
+ViewsController.get('/signup', function (req, res) {
+  var data = {
+    title : 'Sign up',
+    next_link: '/login',
+    text_of_link: 'Log in',
+    script: '/static/js/signup.js'
+  }
+    res.render('login/login.jade', data);
 });
 
 
