65cd085092fcf3c9a9de5ba3df33b85e2d5dad23
Sam Hoang <samhv.ict@gmail.com>
2016-11-30 17:46:08 +0700

    Add Category model

---
+++ module/searchProduct/model/category.js
@@ -0,0 +1,19 @@
+/**
+ * Created by samhv on 11/30/16.
+ */
+'use strict'
+
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    autoIncrement = require('./db');
+
+var CategorySchema = new Schema({
+    _id: Number,
+    name: {
+        type: String
+    }
+});
+
+CategorySchema.plugin(autoIncrement.plugin, 'Category');
+
+module.exports = mongoose.model('Category', CategorySchema);
