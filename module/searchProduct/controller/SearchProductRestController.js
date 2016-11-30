895be7dbb16210e0331a81dc76482d6b5e2d61b1
Sam Hoang <samhv.ict@gmail.com>
2016-11-30 17:59:23 +0700

    Add Search Product Rest Controller

---
+++ module/searchProduct/controller/SearchProductRestController.js
@@ -0,0 +1,16 @@
+/**
+ * Created by samhv on 11/30/16.
+ */
+'use strict'
+
+var express = require('express');
+
+var SearchProductController = express.Router()
+
+var searchProductByCategory = function (req, res) {
+    
+}
+
+SearchProductController.post('')
+
+module.exports = SearchProductController;
